e94412cd223a8ef6d48509e75de94064d81bf7de
jesusvp20 <vegapernettj@gmail.com>
2025-11-27 15:41:15 -0500

    Fix DB import

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Models\User;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Http\Request;
 
 class RegisterController extends Controller
@@ -70,7 +71,7 @@ class RegisterController extends Controller
     {
         try {
             // Debug: probar conexión a la base de datos
-            \DB::connection()->getPdo();
+            DB::connection()->getPdo();
             
             return Validator::make($data, [
                 'name' => ['required', 'string', 'max:255'],
